be81c0a052167bd3c25ff91e6a091b3543aeb2bd
mkov <max-kov@yandex.ru>
2011-10-25 22:30:55 +0300

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, :presence => true,
+                  :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                   :format => { :with => email_regex },
+                   :uniqueness => { :case_sensitive => false }
+  validates :password, :presence => true,
+                      :length => { :within => 6..40 },
+                      :confirmation => true
+
+  before_save :encrypt_password
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6e5668464e1a24821ff5aa2aeada6a514d5349f0b7929ef7aff028f8719cb3442a4c9b9dc1e8fecc1861f4e07d0f0189a813d124ea6cae739bbbbfe3804d7334'
